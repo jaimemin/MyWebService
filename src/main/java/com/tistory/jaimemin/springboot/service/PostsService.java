dd5a36ac1e7cbf35af881058c046d684aa182ef7
jaimemin <jaimemin@gmail.com>
2020-02-25 17:16:22 +0900

    simple page with mustache

--- src/main/java/com/tistory/jaimemin/springboot/service/PostsService.java
+++ src/main/java/com/tistory/jaimemin/springboot/service/PostsService.java
@@ -2,13 +2,16 @@ package com.tistory.jaimemin.springboot.service;
 
 import com.tistory.jaimemin.springboot.domain.posts.Posts;
 import com.tistory.jaimemin.springboot.domain.posts.PostsRepository;
+import com.tistory.jaimemin.springboot.web.dto.PostsListResponseDto;
 import com.tistory.jaimemin.springboot.web.dto.PostsResponseDto;
 import com.tistory.jaimemin.springboot.web.dto.PostsSaveRequestDto;
 import com.tistory.jaimemin.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -40,5 +43,13 @@ public class PostsService {
                         new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
